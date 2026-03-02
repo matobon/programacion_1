b9baf6a37a70c28507ee99644a07f1b179c803d4
matobon <matobon@estudiante.uniajc.edu.co>
2026-03-02 14:48:57 +0000

    ok

--- Parcial_I/Cajero.java
+++ Parcial_I/Cajero.java
@@ -22,22 +22,42 @@ public class Cajero {
 
         if (CLAVE == claveUsuario ){
 
-            System.out.print("Bienvenidos al cajero uniajc");
+            System.out.print("Bienvenidos al cajero Uniajc");
             System.out.print("digite la cantidad a retirar: ");
             retiro = sc.nextInt();
             //si el retiro es mayor a 200000 se cobra 2000
             if(retiro > 200000)  {
                 retiro = retiro + 2000;
 
-                SALDOINICIAL)  { 
-                      nuevoSaldo = 
-            
+                 if ( retiro <= SALDOINICIAL)  { 
+                      if nuevoSaldo = SALDOINICIAL - retiro;
+                      System.out.print( "Su saldo actual es:" + nuevoSaldo + " Muchas Gracias!!!!");
+                 } else { 
+            System.out.print("El valor solicitado mas el costo de la transaccion es mayor al saldo");
+             }
+               } else { 
+                if(retiro <= SALDOINICIAL)  {
+                    nuevoSaldo = SALDOINICIAL - retiro;
+                    System.out.println( "su saldo actual es: " + nuevoSaldo + Muchas Gracias!!!!");
+                     } else { 
+                    System.out.println( "El valor solicitado es mayor al saldo actual.... ");
+                 }
+
+                    }
+                  } else { 
+                System.out.println ("Su clave no es correcta...intenta nuevamente....");
+             }
+                sc.close();
+
+
+                
+
 
 
             }
 
-        }
+        
 
 
     }
-}
+
